9199e79782d7a1a260065f2c764d547d97660164
Ramy Benkov <ramybenkov@Ramys-Laptop.local>
2024-10-04 02:03:22 -0400

    fixing image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header-container"> 
       <img 
-        src="src/assets/image.jpg" 
+        src="/assets/image.jpg" 
         alt="Pokémon Banner" 
         className="banner-image" 
       />
